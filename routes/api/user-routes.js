fdd6c38306a97b805c0a0f6fe982cac269a4ea8a
Robert C <robert.clarady@gmail.com>
2022-01-21 13:05:18 -0600

    updated user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -17,6 +17,7 @@ router.get('/', (req, res) => {
 // GET /api/users/1
 router.get('/:id', (req, res) => {
     User.findOne({
+      attributes: { exclude: ['password'] },  
       where: {
         id: req.params.id
       }
