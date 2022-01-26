ef7b1b0d5ed57540139b90d173232f8cc7e35d94
Robert C <robert.clarady@gmail.com>
2022-01-26 13:30:23 -0600

    added login logout

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -13,6 +14,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
